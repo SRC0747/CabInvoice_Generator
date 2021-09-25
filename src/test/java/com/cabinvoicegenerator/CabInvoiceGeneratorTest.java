d9ef0282e92ea9fcf5f081cf2c0e1f4cf2163ec1
SRC0747 <RjSrc@0747>
2021-09-25 12:57:47 +0530

    Added: Calculating Minimum Fare of Cab Invoice Generator.

--- src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -15,4 +15,13 @@ class CabInvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double minimumFare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, minimumFare, 0.0);
+    }
 }
