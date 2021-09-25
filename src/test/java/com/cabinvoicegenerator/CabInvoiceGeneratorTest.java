cbbef51c360b48e7fbe82dd9574ecd242ccb36d1
SRC0747 <RjSrc@0747>
2021-09-25 21:34:08 +0530

    Added: Refactoring the code by displaying Total no. of Rides, Total Fare and Average Fare per Ride.

--- src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -1,6 +1,7 @@
 package com.cabinvoicegenerator;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CabInvoiceGeneratorTest {
 
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+    public void givenDistanceAndTime_ShouldReturnInvoiceSummary() {
+       InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -18,7 +19,7 @@ class CabInvoiceGeneratorTest {
 
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+       InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double minimumFare = invoiceGenerator.calculateFare(distance, time);
@@ -31,7 +32,10 @@ class CabInvoiceGeneratorTest {
         Ride[] rides = { new Ride(2.0, 5),
                          new Ride(0.1, 1),
                         };
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assertions.assertEquals(30, fare, 0.0);
+        //double fare = invoiceGenerator.calculateFare(rides);
+        //Assertions.assertEquals(30, fare, 0.0);
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assertions.assertEquals(expectedInvoiceSummary, summary);
     }
 }
--- src/test/java/com/cabinvoicegenerator/InvoiceGenerator.java
+++ src/test/java/com/cabinvoicegenerator/InvoiceGenerator.java
@@ -8,16 +8,16 @@ public class InvoiceGenerator {
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILLOMETER + time * COST_PER_TIME;
-        if(totalFare < MINIMUM_FARE)
-            return MINIMUM_FARE;
-        return totalFare;
+        //if(totalFare < MINIMUM_FARE)
+           // return MINIMUM_FARE;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 
-    public double calculateFare(Ride[] rides) {
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride:rides){
             totalFare = this.calculateFare(ride.distance, ride.time);
         }
-        return totalFare;
+        return new InvoiceSummary(rides.length, totalFare);
     }
 }
