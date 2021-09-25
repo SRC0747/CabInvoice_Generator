1d740cd3c59733f512d42fe6ec69c342f8ad49b5
SRC0747 <RjSrc@0747>
2021-09-25 12:48:01 +0530

    Added: Calculating Total Fare of Cab Invoice Generator.

--- src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -1,7 +1,18 @@
 package com.cabinvoicegenerator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CabInvoiceGeneratorTest {
 
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
+    }
 }
---
+++ src/test/java/com/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,11 @@
+package com.cabinvoicegenerator;
+
+public class InvoiceGenerator {
+
+    private static final double MINIMUM_COST_PER_KILLOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+
+    public double calculateFare(double distance, int time) {
+        return distance * MINIMUM_COST_PER_KILLOMETER + time * COST_PER_TIME;
+    }
+}
